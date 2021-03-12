7f165b59e176922318f91048dab0f59df9950ab7
Helena <helenatp@stud.ntnu.no>
2021-03-12 11:33:13 +0100

    feat: change getCoordinates to getLocation and make boardChange return a boolean

--- core/src/com/mygdx/game/model/ships/Ship.java
+++ core/src/com/mygdx/game/model/ships/Ship.java
@@ -3,12 +3,15 @@ package com.mygdx.game.model.ships;
 import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.model.Board;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Ship {
 
     public int getSize();
     public boolean isSunk();
     public Texture getTexture();
-    public int getCoordinates();
-    public void boardChange(Integer xPos, Integer yPos);
+    public ArrayList<List<Integer>> getLocation();
+    public boolean boardChange(Integer xPos, Integer yPos);
 
 }
