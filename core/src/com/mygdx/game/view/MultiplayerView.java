6639490fe6d16560d7921257ab71244a67c4c20a
Isabel <isabelss@ntnu.no>
2021-03-12 10:11:57 +0100

    update on view

--- core/src/com/mygdx/game/view/MultiplayerView.java
+++ core/src/com/mygdx/game/view/MultiplayerView.java
@@ -1,9 +1,27 @@
 package com.mygdx.game.view;
 
+import com.mygdx.game.Game;
+
 public class MultiplayerView extends State {
 
     private int width = Game.WIDTH;
     private int height = Game.HEIGHT;
+
+    private Board board;
+    private Player player;
+    private Cell cell;
+    private DestroyerShip destroyerShip;
+    private CarrierShip carrierShip;
+    private BattleShip battleShip;
+    private CruiserShip cruiserShip;
+    private SubmarineShip submarineShip;
+
+    public MultiplayerView(GameStateManager gsm, Board board){
+        super(gsm) //vet ikke helt hva dette betyr, må finne ut
+
+        map = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+    }
+
 
 
 }
