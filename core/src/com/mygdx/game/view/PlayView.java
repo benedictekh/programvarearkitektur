a413db304cf43407e43f959fc758f3eaca5c0be9
Ane <aned@mac.com>
2021-03-19 17:07:40 +0100

    feat: add a controller to PlayView and draw board and ships

--- core/src/com/mygdx/game/view/PlayView.java
+++ core/src/com/mygdx/game/view/PlayView.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.battleships;
+import com.mygdx.game.controller.BoardController;
 import com.mygdx.game.model.Board;
 import com.mygdx.game.view.GameStateManager;
 import com.mygdx.game.view.State; //endret
@@ -15,12 +17,15 @@ public class PlayView extends  State {
     private Texture board;
     private float x_position;
     private float y_position;
+    private BoardController controller;
+
 
     public PlayView(GameStateManager gsm){
         super(gsm);
 
         background = new Texture("background.PNG");
         board = new Texture("board.PNG");
+        controller = new BoardController( new Board(10, 10));
 
 
     }
@@ -31,7 +36,10 @@ public class PlayView extends  State {
         if(Gdx.input.justTouched()){
             x_position = Gdx.input.getX();
             y_position = Gdx.input.getY();
+            System.out.println("Input position. " + x_position + ", " + y_position);
+            controller.getIndex(x_position, y_position);
         }
+
 
         /*
         Her skal det være en funksjon som sender koordiandene som blir trykket på inn
@@ -51,8 +59,13 @@ public class PlayView extends  State {
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(background,0,0,battleships.WIDTH,battleships.HEIGHT);
-        sb.draw(board,0,0,battleships.WIDTH,battleships.HEIGHT);
+        //sb.draw(board,0,0,battleships.WIDTH,battleships.HEIGHT);
+
+
+
         sb.end();
+        controller.getBoard().drawBoard();
+        controller.getBoard().drawShips();
 
     }
 
@@ -60,4 +73,6 @@ public class PlayView extends  State {
     public void dispose() {
 
     }
+
+
 }
