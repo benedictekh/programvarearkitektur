aa44d4a509ce2fe19e3b87e40950dbf5a2c62458
Ane <aned@mac.com>
2021-03-19 17:05:56 +0100

    feat: create method for "translate" from coordinate to index

--- core/src/com/mygdx/game/controller/BoardController.java
+++ core/src/com/mygdx/game/controller/BoardController.java
@@ -2,14 +2,39 @@ package com.mygdx.game.controller;
 
 import com.mygdx.game.model.Board;
 
+import java.util.ArrayList;
+
 public class BoardController extends Controller{
 
-    BoardController(Board board) {
+    public BoardController(Board board) {
         super(board);
     }
 
     @Override
     public void update(float dt) {
 
+    }
+
+    public ArrayList<Integer> getIndex(float x_pos, float y_pos){
+        //finds the position on the board
+        x_pos = x_pos -board.getSidemargin();
+        y_pos = y_pos -board.getSidemargin();
+
+
+        ArrayList<Integer>  indexes = new ArrayList<>();
+       float t_width = board.getWidth();
+       //float t_height = board.getTexture().getHeight();
+       float cell_width = t_width / board.getBoard().size();
+       float cell_height = t_width / board.getBoard().size();
+
+
+       indexes.add((int) (x_pos / cell_width));
+       indexes.add((int) (y_pos / cell_height));
+       System.out.println(indexes);
+       return indexes;
+    }
+
+    public Board getBoard(){
+        return this.board;
     }
 }
