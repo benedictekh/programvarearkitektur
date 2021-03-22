2159aca1c4d68aaddfe327e8bb17986d837ae651
Live Pettersdatter Melkild <livepm@stud.ntnu.no>
2021-03-22 14:06:17 +0100

    koble sammen ulike views

--- core/src/com/mygdx/game/view/MenuView.java
+++ core/src/com/mygdx/game/view/MenuView.java
@@ -2,28 +2,47 @@ package com.mygdx.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.mygdx.game.battleships;
+
+import java.awt.Button;
 
 public class MenuView extends State{
 
     Texture background;
     Texture playBtn;
+    Texture initButton;
 
     public MenuView(GameStateManager gsm) {
         super(gsm);
 
         background = new Texture("background.PNG");
-        playBtn = new Texture("plybtn.PNG");
+        playBtn = new Texture("button_2.png");
+        initButton = new Texture("initButton.png");
 
     }
 
     @Override
     protected void handleInput() {
         if(Gdx.input.justTouched()){
-            gsm.set(new PlayView(gsm));
-        }
+            //om den grønne knappen blir trykket på kommer man til board-et
+            if (Gdx.input.getY()<= battleships.HEIGHT / 2) {
+                gsm.set(new PlayView(gsm));
+            }
 
+        }
+        if(Gdx.input.justTouched()){
+            //om den blå knappen trykkes på kommer man til setting/tutorial + feedback
+            if (Gdx.input.getY()> battleships.HEIGHT / 2) {
+                gsm.set(new SettingsView(gsm));
+            }
+
+        }
     }
 
     @Override
@@ -36,9 +55,10 @@ public class MenuView extends State{
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.draw(background, 0, 0, battleships.WIDTH, battleships.HEIGHT);
-        sb.draw(playBtn,(battleships.WIDTH/2-100),battleships.HEIGHT/2,200 ,200 );
-        sb.end();
 
+        sb.draw(playBtn,(battleships.WIDTH/2-100),battleships.HEIGHT-200,200 ,100);
+        sb.draw(initButton,(battleships.WIDTH/2-100),100,200,100);
+        sb.end();
     }
 
     @Override
