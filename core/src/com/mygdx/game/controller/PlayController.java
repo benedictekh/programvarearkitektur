b5f9e126379026bea9e6e65220d7ba0874856447
Live Pettersdatter Melkild <livepm@stud.ntnu.no>
2021-04-20 13:00:01 +0200

    trying

--- core/src/com/mygdx/game/controller/PlayController.java
+++ core/src/com/mygdx/game/controller/PlayController.java
@@ -116,7 +116,6 @@ public class PlayController extends Controller{
                     }
                 };
                     executor.schedule(task, 3, TimeUnit.SECONDS);
-
             }
         }
         else{
